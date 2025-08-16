714ae179fb8cae8526adcc6d8c67b207d4eefc04
Dom Rodriguez <shymega@shymega.org.uk>
2025-08-17 00:58:29 +0100

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    hyprland = {
+      url = "github:hyprwm/Hyprland?ref=v0.55.2";
+    };
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins?ref=v0.53.0";
+      inputs.hyprland.follows = "hyprland"; # Prevents version mismatch.
+    };
+    snappy-switcher = {
+      url = "github:OpalAayan/snappy-switcher";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    win2k-plymouth.url = "github:Win2K-for-Unix/Win2K-Plymouth";
+  };
+
+  outputs = inputs: let
+    inherit (inputs) self nixpkgs;
+  in {};
+}
