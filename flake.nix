807b69feda385bd61133dbdd636848bbd467d18a
Dom Rodriguez <shymega@shymega.org.uk>
2026-02-27 19:18:19 +0000

    chore: Uprev to Hyprland v0.54.0

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     hyprland = {
-      url = "github:hyprwm/Hyprland?ref=v0.53.3";
+      url = "github:hyprwm/Hyprland?ref=v0.54.0";
     };
     hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
+      url = "github:hyprwm/hyprland-plugins?ref=v0.53.0";
       inputs.hyprland.follows = "hyprland"; # Prevents version mismatch.
     };
     stylix = {
