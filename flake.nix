9391d74a49124daee8abc55fcb65b0d8afa8000a
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-29 22:50:18 +0100

    chore: Bump Hyprland

--- flake.nix
+++ flake.nix
@@ -2,15 +2,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
     hyprland = {
-      url = "github:hyprwm/Hyprland?tag=v0.54.2";
+      url = "github:hyprwm/Hyprland?ref=v0.54.3";
     };
-    stylix = {
-      url = "github:danth/stylix/release-25.11";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins?ref=v0.53.0";
+      inputs.hyprland.follows = "hyprland"; # Prevents version mismatch.
+    };
+    snappy-switcher = {
+      url = "github:OpalAayan/snappy-switcher";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    base16-schemes.url = "github:SenchoPens/base16.nix";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     win2k-plymouth.url = "github:Win2K-for-Unix/Win2K-Plymouth";
