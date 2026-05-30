bccce08bd438d4420b810aacc0fa724e2fe77157
Dom Rodriguez <shymega@shymega.org.uk>
2026-05-30 20:59:46 +0100

    feat: Uprev to NixOS 26.05
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
     hyprland = {
       url = "github:hyprwm/Hyprland?ref=v0.55.2";
     };
@@ -13,7 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     quickshell = {
